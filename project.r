f14a6f071d02638732684579fa0e51067bbf3167
depa-tto <antonio.depatto@studenti.unimi.it>
2025-02-21 12:00:38 +0200

    food companies

--- project.r
+++ project.r
@@ -1,1 +1,23 @@
-library(quantmod)  
+library(quantmod)
+library(tidyverse)
+library(dplyr)
+
+# food companies
+
+nestle <- getSymbols("NESN.SW", src = "yahoo", from = "2020-01-01", to = "2022-12-31", auto.assign = FALSE)
+nestle <- nestle$NESN.SW.Close
+head(nestle)
+
+unilever <- getSymbols("HINDUNILVR.NS", src = "yahoo", from="2020-01-01", to = "2022-12-31", auto.assign = FALSE)
+unilever <- unilever$HINDUNILVR.NS.Close
+head(unilever)
+
+danone <- getSymbols("BN.PA", src = "yahoo", from="2020-01-01", to = "2022-12-31", auto.assign = FALSE)
+danone <- danone$BN.PA.Close
+head(danone)
+
+bonduelle <- getSymbols("BON.PA", src = "yahoo", from="2020-01-01", to = "2022-12-31", auto.assign = FALSE)
+bonduelle <- bonduelle$BON.PA.Close
+head(bonduelle)
+
+
